350c40d589e269eba536275d2d61778d87b7c2dd
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-04-24 23:59:25 +0530

    added input element and  a button to add food

--- react-update-array/src/MyComponent.jsx
+++ react-update-array/src/MyComponent.jsx
@@ -7,12 +7,20 @@ function MyComponent() {
     const [foods, setFoods] = useState(["noodles","pizza","kfc"]);
     /* let's create one handler function to add an element to this array
     and another one to remove an element */
+    function handleAddFoods() {
+
+    }
+    function handleRemoveFoods() {
+
+    }
     return(
         <div>
             <h1>List of foods</h1>
             <ul>
                 {foods.map((food,index) => <li key={index}> {food} </li>)}
             </ul>
+            <input type="text" id="food-input" placeholder="Enter food name"/>
+            <button onClick={handleAddFoods} > Add food </button>
         </div>
     );
 }
